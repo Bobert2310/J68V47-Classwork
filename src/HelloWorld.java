67794c078e7df725850fa08290d05c1e9ec314d5
Scott <30088036@nescol.ac.uk>
2023-08-30 11:21:04 +0100

    .

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * My First Program
+ * @author Scott Robson
+ * @version 1
+ */
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
