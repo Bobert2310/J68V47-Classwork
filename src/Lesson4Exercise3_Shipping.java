832cccceca928a8f658da95d3f693cb735432233
Scott <30088036@nescol.ac.uk>
2023-09-20 11:58:41 +0100

    .

--- src/Lesson4Exercise3_Shipping.java
+++ src/Lesson4Exercise3_Shipping.java
@@ -9,10 +9,15 @@ public class Lesson4Exercise3_Shipping {
         System.out.println("Enter the total purchase amount: ");
         double PurA = input.nextDouble();
 
-        System.out.format("\nShipping Cost is: £%.2f" + Shipping);
+        System.out.format("\nShipping Cost is: £%.2f", Shipping);
+        Total = PurA;
+        if (PurA < 50) {
 
-        Total = PurA + Shipping;
-        System.out.format("\nyour final total is: £%.2f" + Total);
+            Total = PurA + Shipping;
+
+        }
+
+        System.out.format("\nyour final total is: £%.2f", Total);
 
     }
 }
