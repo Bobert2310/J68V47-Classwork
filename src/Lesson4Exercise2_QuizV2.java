a94f0253f38e60edd5f96920828ccf89de43e80f
Scott <30088036@nescol.ac.uk>
2023-09-27 11:55:47 +0100

    .

--- src/Lesson4Exercise2_QuizV2.java
+++ src/Lesson4Exercise2_QuizV2.java
@@ -1,4 +1,3 @@
-import javax.imageio.plugins.tiff.FaxTIFFTagSet;
 import java.util.Scanner;
 
 public class Lesson4Exercise2_QuizV2 {
@@ -7,22 +6,27 @@ public class Lesson4Exercise2_QuizV2 {
         System.out.println("What is the capital of Spain:");
         String Answer1 = input.nextLine();
 
-        System.out.println(Answer1.toLowerCase().contains("madrid")); //true
-        if (Answer1.equals(false));
-        System.out.println("Sorry, the correct answer is Madrid.");
-
+        if (Answer1.toLowerCase().contains("madrid")) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Sorry, the correct answer is Madrid.");
+        }
         System.out.println("What is the capital of the UK:");
         String Answer2 = input.nextLine();
 
-        System.out.println(Answer2.toLowerCase().contains("london")); //true
-        if (Answer2.equals(false));
-        System.out.println("Sorry, the correct answer is London.");
+        if (Answer2.toLowerCase().contains("london")) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Sorry, the correct answer is London.");
+        }
 
         System.out.println("What is the capital of Italy:");
         String Answer3 = input.nextLine();
 
-        System.out.println(Answer3.toLowerCase().contains("rome")); //true
-        if (Answer3.equals(false));
-        System.out.println("Sorry, the correct answer is Rome.");
+        if (Answer3.toLowerCase().contains("rome")) {
+            System.out.println("Correct!");
+        } else {
+            System.out.println("Sorry, the correct answer is Rome.");
+        }
     }
 }
