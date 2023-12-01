0bed8f9d519210ee378261d00cf60f91a081e7a6
scott <scottrob2310@gmail.com>
2023-12-01 20:54:35 +0000

    .

--- src/Lesson11Exercise3_ReadingCalculating.java
+++ src/Lesson11Exercise3_ReadingCalculating.java
@@ -1,2 +1,18 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Lesson11Exercise3_ReadingCalculating {
+    public static void main(String[] args) {
+        try ( Scanner in = new Scanner( new FileReader("FileOfIntegers.txt") ) ) {
+            String text = in.nextLine();
+            double num = in.nextDouble();
+            while ( in.hasNextLine() ) {
+                System.out.println( in.nextLine() );
+            }
+
+            System.out.println(text);
+        } catch (IOException e) {
+            System.out.println("Error occurred reading from file: " + e.toString());
+        }
+    }
 }
