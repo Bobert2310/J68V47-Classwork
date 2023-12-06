08481bbe10915187689f20774c387a8f4b0ed5f9
Scott <30088036@nescol.ac.uk>
2023-12-06 15:08:48 +0000

    .

--- src/Lesson11Exercise3_ReadingCalculating.java
+++ src/Lesson11Exercise3_ReadingCalculating.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Lesson11Exercise3_ReadingCalculating {
     public static void main(String[] args) {
         try ( Scanner in = new Scanner( new FileReader("FileOfIntegers.txt") ) ) {
-            String text = in.nextLine();
-            double num = in.nextDouble();
-            while ( in.hasNextLine() ) {
-                System.out.println( in.nextLine() );
+            double Sum = 0;
+            while ( in.hasNextDouble()) {
+                double num = in.nextDouble();
+                Sum += num;
             }
 
-            System.out.println(text);
+            System.out.println("Sum of all numbers in this file: " + Sum);
         } catch (IOException e) {
             System.out.println("Error occurred reading from file: " + e.toString());
         }
